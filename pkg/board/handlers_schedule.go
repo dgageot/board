cc7f1db471e53c303ced6a4b92e0b92a6977c2e4
David Gageot <david.gageot@docker.com>
2026-04-04 22:33:32 +0200

    Simplify handleListSchedule with range and append
    Assisted-By: docker-agent

--- pkg/board/handlers_schedule.go
+++ pkg/board/handlers_schedule.go
@@ -59,9 +59,9 @@ func (b *Board) handleListSchedule(w http.ResponseWriter, _ *http.Request) {
 		return
 	}
 
-	result := make([]scheduleCardResponse, len(cards))
-	for i, c := range cards {
-		result[i] = toScheduleCard(c)
+	var result []scheduleCardResponse
+	for _, c := range cards {
+		result = append(result, toScheduleCard(c))
 	}
 	writeJSON(w, result)
 }
